d2755efad4f9fe5894e262fa0edafcc3d3f915b7
BruceShan <1057721266@qq.com>
2018-07-21 15:18:09 +0800

    荷兰国旗问题  三划分 随机 小数组切换成插入插入排序的快排

--- src/main/java/array/QuickSort.java
+++ src/main/java/array/QuickSort.java
@@ -5,15 +5,45 @@ package array;
  * Description :  快排 荷兰国旗问题
  */
 public class QuickSort {
-    // 利用荷兰国旗问题优化的快排
-    public void QS(int[] arr,int l,int r){
-        if(l >r) return;
-
-        swap(arr, l + (int) (Math.random() * (r - l + 1)), r);
-       // int[] p = partition(arr,)
+    // 对外统一快排调用 入口
+    public void quickSort(int[] arr) {
+        if (arr == null || arr.length == 1) {
+            return;
+        }
+        QS(arr, 0, arr.length - 1);
     }
+
+    // 利用荷兰国旗问题优化的快排
+    public void QS(int[] arr, int l, int r) {
+        if (l > r) return;
+
+        int M = 10;   // 切换至插入排序的阈值 5到15之间比较合理
+        if (l + M >= r) insertSort(arr, l, r);
+        // Math.random() * N  取得是 [0 N) 之间的小数
+        swap(arr, l + (int) (Math.random() * (r - l + 1)), r);  // 随机
+        int[] p = partition(arr, l, r);
+        QS(arr, l, p[0] - 1);
+        QS(arr, p[1] + 1, r);
+    }
+
+    // 荷兰国旗问题改进的快排的划分
+    public int[] partition(int[] arr, int l, int r) {
+        int less = l - 1;
+        int more = r + 1;
+        while (l < more) {
+            if (arr[l] < arr[r]) {
+                swap(arr, ++less, l++);
+            } else if (arr[l] > arr[r]) {
+                swap(arr, --more, l);
+            } else {
+                l++;
+            }
+        }
+        return new int[]{less + 1, more - 1};   // 返回大于小于的边界
+    }
+
     // 荷兰国旗问题
-    public  int[] partition(int[] arr, int l, int r, int p) {
+    public int[] partition(int[] arr, int l, int r, int p) {
         int less = l - 1;
         int more = r + 1;
         while (l < more) {
@@ -25,31 +55,46 @@ public class QuickSort {
                 l++;
             }
         }
-        return new int[] { less + 1, more - 1 };   // 返回大于小于的边界
+        return new int[]{less + 1, more - 1};   // 返回大于小于的边界
     }
 
-    public void  swap(int[] arry, int i ,int j){
-        if(arry[i]!=arry[j]){
-            arry[i] = arry[i]^arry[j];
-            arry[j] = arry[i]^arry[j];
-            arry[i] = arry[i]^arry[j];
+    // 插入排序  常数项占优势
+    public void insertSort(int[] arr, int l, int r) {
+        if (arr == null || arr.length == 1) return;
+
+        for (int i = l + 1; i <= r; i++) {
+            int j = i - 1;
+            while (j >= l && arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+                j--;
+            }
+        }
+        return;
+    }
+
+
+    //  通过异或交换两值
+    public void swap(int[] arr, int i, int j) {
+        if (arr[i] != arr[j]) {
+            arr[i] = arr[i] ^ arr[j];
+            arr[j] = arr[i] ^ arr[j];
+            arr[i] = arr[i] ^ arr[j];
         }
     }
 
     // 数组打印
-    public void arrayPrint(int[]  array){
+    public void arrayPrint(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]+" ");
+            System.out.print(array[i] + " ");
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
         QuickSort solution = new QuickSort();
-        int[]  array = {2,3,5,6,2,1,3,7};
-        //solution.DutchFlag(array);
-        solution.partition(array,0,array.length-1,3);
+        int[] array = {2, 3, 5, 6, 2, 9, 3, 7};
 
+        solution.QS(array, 0, array.length - 1);
         solution.arrayPrint(array);
     }
 
